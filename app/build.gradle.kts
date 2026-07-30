985c036e826f0ac900bbc7f99921fc9b2fab8561
kevinlj02-arch <kevinlj02@gmail.com>
2026-07-30 11:45:45 -0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "0.3.0-demo"
+        versionName = "0.4.0-demo"
 
         buildConfigField(
             "String",
@@ -44,6 +44,7 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.activity:activity-compose:1.9.1")
     implementation(platform("androidx.compose:compose-bom:2024.09.00"))
     implementation("androidx.compose.ui:ui")
